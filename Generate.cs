8a39595afbb8eb6f810df9c2f3122cb75f729da0
viniciusrc15 <vinicius1595@gmail.com>
2017-10-26 21:40:31 -0200

    Constrolando monstros

---
+++ Generate.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+ 
+public class Generate : MonoBehaviour
+{
+    public GameObject monstro;
+     
+    // Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("CreateObstacle", 1f, 1.5f);
+    }
+     
+    void CreateObstacle()
+    {
+        Instantiate(monstro);
+    }
+}
